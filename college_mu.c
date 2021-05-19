d32353f9f85eca125ae2ac71eaa528a0575154c9
adwaitrao7000 <71839093+adwaitrao7000@users.noreply.github.com>
2021-05-19 17:06:33 +0530

    change in code and name

---
+++ college_mu.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+void loadinganimation();
+int main()
+{
+    loadinganimation();
+    system("color 0f");
+    system("cls");
+    system("pause");
+    system("cls");
+    printf("\n\n");
+    printf("\t\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4 \n");
+    printf("\t\4\t                                                   \t\4\n");
+    printf("\t\4\t College Management Utility - version 0.0001 alpha \t\4\n");
+    printf("\t\4\t                                                   \t\4\n");
+    printf("\t\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\n");
+    system("pause");
+}
+
+void loadinganimation()
+{
+    int a, i, j;
+    system("color 0E");
+    printf("\n\n\n\n\n\n\n\n\n\n\t\t\t\Loading ");
+    for (i = 0; i<=20; i++)
+    {
+        for (j = 0; j<10000000; j++)
+        {
+            a = j;
+        }
+            printf("%c", 219);
+    }
+    return 0;
+}
