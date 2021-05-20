0b15f964d35d8413cb819c8cb6df6dcf21ff875a
adwaitrao7000 <71839093+adwaitrao7000@users.noreply.github.com>
2021-05-20 20:47:39 +0530

    Added Login screen

--- college_mu.c
+++ college_mu.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+#include <string.h>
+//#include <type.h>
+#include <windows.h>
 void loadinganimation();
+void title();
+void adminlogin();
 int main()
 {
     loadinganimation();
-    system("color 0f");
     system("cls");
-    system("pause");
-    system("cls");
-    printf("\n\n");
-    printf("\t\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4 \n");
-    printf("\t\4\t                                                   \t\4\n");
-    printf("\t\4\t College Management Utility - version 0.0001 alpha \t\4\n");
-    printf("\t\4\t                                                   \t\4\n");
-    printf("\t\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\n");
-    system("pause");
+    adminlogin();
 }
 
 void loadinganimation()
 {
     int a, i, j;
-    system("color 0E");
-    printf("\n\n\n\n\n\n\n\n\n\n\t\t\tLoading ");
+    system("color 0A");
+    printf("\n\n\n\n\n\n\n\n\n\n\t\t\t\Loading ");
     for (i = 0; i<=20; i++)
     {
         for (j = 0; j<10000000; j++)
@@ -32,4 +27,50 @@ void loadinganimation()
             printf("%c", 219);
     }
     return 0;
+}
+
+void title()
+{
+    system("color 0f");
+    //system("pause");
+    system("cls");
+    printf("\n\n");
+    printf("\t\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4 \n");
+    printf("\t\4\t                                                   \t\4\n");
+    printf("\t\4\tCollege Management Utility - version 0.0001 alpha \t\4\n");
+    printf("\t\4\t                                                   \t\4\n");
+    printf("\t\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\n");
+    printf("\n\n\n\n\t\t1. Option 1\n");
+    printf("\t\t2. Option 2\n");
+    printf("\t\t3. Option 3\n");
+    printf("\t\t4. Option 4\n");
+    printf("\t\t5. Option 5\n");
+    system("pause");
+}
+
+void adminlogin()
+{
+    char username[20];
+    char password[20];
+    char rusername[20] = {"admin"};
+    char rpassword[20] = {"admin"};
+    L:
+    title();
+    printf("\n\n\t\tUsername: ");
+    gets(username);
+    printf("\n\t\tPassword: ");
+    gets(password);
+
+    if(strcmp(rusername, username)!=0 && strcmp(rpassword, password)!=0)
+    {
+        printf("Wrong Username Or Password, Please Try Again And\nInput Credentials Carefully.");
+        system("pause");
+        system("cls");
+        goto L;
+    }
+    else if(strcmp(rusername, username)==0 && strcmp(rpassword, password)==0)
+    {
+        printf("Login Successful!.\n");
+        //function for choise redirecting to a function by switch statement
+    }
 }
