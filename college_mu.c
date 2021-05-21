b26c56a48102f0ea0ac6baa9561470f7dc48b220
adwaitrao7000 <71839093+adwaitrao7000@users.noreply.github.com>
2021-05-21 12:36:34 +0530

    optimized Login page with limited attempts

--- college_mu.c
+++ college_mu.c
@@ -1,55 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-void welcome_screen();
+#include <conio.h>
+//#include <type.h>
+#include <windows.h>
 void loading_animation();
-void main(){
+void title();
+void admin_login();
+int main()
+{
     loading_animation();
-    login();
-}
-void login(){
-    
-    int e=5;
-    char username[20];
-    char password[20];
-    char real_username[20]="heramb";
-    char real_password[20]="4270";
-    for(e=5;e>=0;e--){
     system("cls");
-    welcome_screen();
-    system("color 3C");
-    printf("\n\n\t\t\t Enter UserName :- ");
-    scanf("%s",username);
-    printf("\n\n\t\t\t Enter Password :- ");
-    scanf("%s",password);
-       if(strcmp(username,real_username)==0  &&  strcmp(password,real_password)==0){
-           printf("\n\n\n\n\t\t\t\t ........Login Successfull........");
-           getch();
-           break;
-       }else{
-           printf("\n\n\n\t\t\t Password Is Incorrect (Try Again) ");
-           printf("\n\n\n\t\t\t\t\t Tries Remaining :-  %d (Press Any Key to Continue)",e);
-           getch();
-       }
-    }
+    admin_login();
 }
-void welcome_screen(){
+
+void loading_animation()
+{
+    int a, i, j;
+    system("color 0A");
+    printf("\n\n\n\n\n\n\n\n\n\n\t\t\t\Loading Please Wait ");
+    for (i = 0; i<=20; i++)
+    {
+        for (j = 0; j<10000000; j++)
+        {
+            a = j;
+        }
+            printf("%c", 219);
+    }
+    return 0;
+}
+
+void title()
+{
     system("color 0f");
     system("cls");
-    printf("\n\n\n");
+    printf("\n\n");
     printf("\t\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4 \n");
     printf("\t\4\t                                                   \t\4\n");
-    printf("\t\4\t College Management Utility - version 0.0001 alpha \t\4\n");
+    printf("\t\4\tCollege Management Utility - version 0.0001 alpha \t\4\n");
     printf("\t\4\t                                                   \t\4\n");
-    printf("\t\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4 \n");
+    printf("\t\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\4\n");
 }
-void loading_animation(){
-    int i=0,a,j;
-    system("color 0A");
-    printf("\n\n\n\n\n\n\n\n\n\n\t\tLoading Please Wait ");
-    for( i=0;i<=20;i++){
-       for(j=0;j<=100000000;j++){
-         a=j;
-       }
-    printf("%c",219);
+
+void admin_login()
+{
+    char username[20];
+    char password[20];
+    char rusername[20] = {"admin"};
+    char rpassword[20] = {"admin"};
+    title();
+    system("color 71");
+
+    for (int e = 4; e>=0; e--)
+    {
+        printf("\n\t-----------------------------------------------------------------\n");
+        printf("\t\t                  Login Page\n");
+        printf("\t-----------------------------------------------------------------\n");
+        printf("\n\t\t\t Username: ");
+        gets(username);
+        printf("\n\t\t\t Password: ");
+        gets(password);
+
+        if(strcmp(username, rusername)==0 && strcmp(password, rpassword)==0)
+        {
+            printf("\n\n\t\t\t........Login Successfull........");
+            getch();
+        }
+        else
+        {
+            printf("\n\t\t\tIncorrect Username Or Password\n\t\t\tPlease Try Again\n\n\t\t\t %d Attempt(s) Remaining", e);
+            getch();
+            system("cls");
+
+        }
     }
+    system("color 0c");
+    printf("\n\n\t\tYou Have Used Maximum Number Of Attempts! Please Try Again Later");
+    getch();
+    exit(0);
 }
