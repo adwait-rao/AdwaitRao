dd7de0c5ca5de5d5b64b0c4e5d79002118ccc911
Heramb4270 <84409645+Heramb4270@users.noreply.github.com>
2021-05-25 20:51:54 +0530

    Update college_mu.c

--- college_mu.c
+++ college_mu.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
-//#include <type.h>
 #include <windows.h>
-
 
 void loading_animation();
 void title();
 void admin_login();
 void main_menu();
-
 void add_rec();
+void search_rec();
 void view_records();
 
-void gotoxy(short x, short y)
+void gotoxy(int x, int y)
 {
     COORD pos = {x, y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
@@ -31,16 +29,17 @@ void loading_animation()
 {
     int a, i, j;
     system("color 0A");
-    printf("\n\n\n\n\n\n\n\n\n\n\t\t\tLoading Please Wait ");
-    for (i = 0; i<=20; i++)
+    gotoxy(50, 17);
+    printf("Loading Please Wait ");
+    gotoxy(50, 19);
+    for (i = 0; i <= 22; i++)
     {
-        for (j = 0; j<10000000; j++)
+        for (j = 0; j < 100000000; j++)
         {
             a = j;
         }
-            printf("%c", 219);
+        printf("%c", 178);
     }
-    return 0;
 }
 
 void title()
@@ -63,8 +62,8 @@ void admin_login()
     char rpassword[20] = {"admin"};
     title();
     system("color 0F");
-
-    for (int e = 4; e>=0; e--)
+    int e;
+    for (e = 4; e >= 0; e--)
     {
         printf("\n\t-----------------------------------------------------------------\n");
         printf("\t\t                  Login Page\n");
@@ -74,7 +73,7 @@ void admin_login()
         printf("\n\t\t\t Password: ");
         gets(password);
 
-        if(strcmp(username, rusername)==0 && strcmp(password, rpassword)==0)
+        if (strcmp(username, rusername) == 0 && strcmp(password, rpassword) == 0)
         {
             printf("\n\n\t\t\t........Login Successfull........");
             getch();
@@ -102,10 +101,8 @@ struct student
     char Last_Name[20];
     char Contact_no[15];
     char email[35];
-    //char address[40];
-    //char clas[15];
-    unsigned int enr_num;
-    char branch[20];
+    char Enrollment[10];
+    char department1[30];
 };
 struct student p;
 
@@ -113,242 +110,281 @@ void main_menu()
 {
     int choise;
     system("cls");
-    //title();
-    r:
-        printf("\n\t-----------------------------------------------------------------\n");
-        printf("\t\t                   Main Menu\n");
-        printf("\t-----------------------------------------------------------------\n");
-        printf("\n\t\t\t1. Add Records");
-        printf("\n\t\t\t2. View Records");
-        printf("\n\t\t\t3. Delete Record");
-        printf("\n\t\t\t4. Edit Record");
-        printf("\n\t\t\t5. Sort Records List");
-        printf("\n\t\t\t6. Exit");
+//title();
+r:
+    printf("\n\t-----------------------------------------------------------------\n");
+    printf("\t\t                   Main Menu\n");
+    printf("\t-----------------------------------------------------------------\n");
+    printf("\n\t\t\t1. Add Records");
+    printf("\n\t\t\t2. View Records");
+    printf("\n\t\t\t3. Search Record");
+    printf("\n\t\t\t4. Exit");
 
-        printf("\n\n\t\t\tEnter The Option: ");
-        scanf("%d", &choise);
-        switch(choise)
-        {
-        case 1:
-            add_rec();
-            break;
+    printf("\n\n\t\t\tEnter The Option: ");
+    scanf("%d", &choise);
+    switch (choise)
+    {
+    case 1:
+        add_rec();
+        break;
 
-        case 2:
-            view_records();
-            break;
+    case 2:
+        view_records();
+        break;
 
+    case 3:
+        search_rec();
+        break;
 
-        default:
-            printf("\n\t\t\tInvalid Option!");
-            getch();
-            system("cls");
-            goto r;
-            break;
-        }
+    case 4:
+        printf("\n\n\t\t\t Thanks You For Using Our Software !!!!!!!");
+        getch();
+        exit (0);   
+
+    default:
+        printf("\n\t\t\tInvalid Option!");
+        getche();
+        system("cls");
+        goto r;
+        break;
+    }
 }
-
 
 void add_rec()
 {
     FILE *data;
-    data = fopen("studnt.dat", "a");
+    data = fopen("studnt.bin", "ab");
     int valid;
     char ans;
     system("cls");
-//************************************First Name******************************************
-    name:
-        printf("\n\t-----------------------------------------------------------------\n");
-        printf("\t\t                   Add Record\n");
-        printf("\t-----------------------------------------------------------------\n");
+    //*************First Name***************
+name:
+    printf("\n\t-----------------------------------------------------------------\n");
+    printf("\t\t                   Add Record\n");
+    printf("\t-----------------------------------------------------------------\n");
 
-        printf("\n\t\t\tFirst Name: ");
-        scanf("%s", &p.First_Name);
+    printf("\n\t\t\tFirst Name: ");
+    scanf("%s", &p.First_Name);
 
-        if(strlen(p.First_Name)>20 || strlen(p.First_Name)<2)
+    if (strlen(p.First_Name) > 20 || strlen(p.First_Name) < 2)
+    {
+        printf("\n\t\t\tPlease Enter Your first name minimum 3 characters \n\t\t\tOr Maximum 20 characters long");
+        getch();
+        system("cls");
+        goto name;
+    }
+    else
+
+    {
+        int i;
+        for (i = 0; i < strlen(p.First_Name); i++)
         {
-            printf("\n\t\t\tPlease Enter Your first name minimum 3 characters \n\t\t\tOr Maximum 20 characters long");
-            getch();
+            if (isalpha(p.First_Name[i]))
+            {
+                valid = 1;
+            }
+            else
+            {
+                valid = 0;
+                break;
+            }
+        }
+
+        if (!valid)
+        {
+            printf("\n\t\t\tThe First Name Contains Invalid Characters (Numbers, Symbols, etc)\nPlease try Again.");
             system("cls");
             goto name;
         }
-        else
-        {
-            for(int i = 0; i<strlen(p.First_Name); i++)
-            {
-                if(isalpha(p.First_Name[i]))
-                {
-                    valid = 1;
-                }
-                else
-                {
-                    valid = 0;
-                    break;
-                }
-            }
+    }
+    p.First_Name[0] = toupper(p.First_Name[0]);
 
-            if(!valid)
+    //***********Last Name**************
+lastname:
+    printf("\n\t\t\tLast Name: ");
+    scanf("%s", &p.Last_Name);
+    int i;
+    if (strlen(p.Last_Name) > 20 || strlen(p.Last_Name) < 2)
+    {
+        printf("\n\t\t\tPlease Enter Your last name minimum 3 characters \nOr Maximum 20 characters long");
+        getch();
+        system("cls");
+        goto lastname;
+    }
+    else
+    {
+        for (i = 0; i < strlen(p.Last_Name); i++)
+        {
+            if (isalpha(p.Last_Name[i]))
             {
-                printf("\n\t\t\tThe First Name Contains Invalid Characters (Numbers, Symbols, etc)\nPlease try Again.");
-                system("cls");
-                goto name;
+                valid = 1;
+            }
+            else
+            {
+                valid = 0;
+                break;
             }
         }
-        p.First_Name[0] = toupper(p.First_Name[0]);
 
-//**********************************Last Name***************************************
-    lastname:
-        printf("\n\t\t\tLast Name: ");
-        scanf("%s", &p.Last_Name);
-
-        if (strlen(p.Last_Name)>20 || strlen(p.Last_Name)<2)
+        if (!valid)
         {
-            printf("\n\t\t\tPlease Enter Your last name minimum 3 characters \nOr Maximum 20 characters long");
+            printf("\n\t\t\tThe Last Name Contains Invalid Characters (Numbers, Symbols, etc)\nPlease try Again.");
             getch();
             system("cls");
             goto lastname;
         }
-        else
-        {
-            for(int i = 0; i<strlen(p.Last_Name); i++) {
-                if(isalpha(p.Last_Name[i]))
-                {
-                    valid = 1;
-                }
-                else
-                {
-                    valid = 0;
-                    break;
-                }
-            }
+    }
+    p.Last_Name[0] = toupper(p.Last_Name[0]);
 
-            if(!valid)
+    //***********Gender*****************
+gender:
+    printf("\n\t\t\tGender(M/F): ");
+    scanf("%s", &p.Gender);
+    p.Gender[0] = toupper(p.Gender[0]);
+
+    //***********AGE*************
+age:
+    printf("\n\t\t\tAge: ");
+    scanf("%d", &p.age);
+
+    if (isalpha(p.age))
+    {
+        printf("\n\t Wrong Input Enter Again!!!");
+    }
+
+    //**************Contact Number****************
+contact:
+    printf("\n\t\t\tContact Number: ");
+    scanf("%s", &p.Contact_no);
+
+    if (strlen(p.Contact_no) != 10)
+    {
+        printf("\n\t\t\tPlease Enter Atleast 10 digit for contact number");
+        getch();
+        system("cls");
+        goto contact;
+    }
+    else if (strlen(p.Contact_no) == 10)
+    {
+        for (i = 0; i < strlen(p.Contact_no); i++)
+        {
+            if (!isalpha(p.Contact_no[i]))
             {
-                printf("\n\t\t\tThe Last Name Contains Invalid Characters (Numbers, Symbols, etc)\nPlease try Again.");
-                getch();
-                system("cls");
-                goto lastname;
+                valid = 1;
+            }
+            else
+            {
+                valid = 0;
+                break;
             }
         }
-        p.Last_Name[0] = toupper(p.Last_Name[0]);
-
-
-//******************************Gender************************************************
-    gender:
-        printf("\n\t\t\tGender(M/F): ");
-        scanf("%s", &p.Gender);
-        p.Gender[0]= toupper(p.Gender[0]);
-
-//******************************AGE************************************
-    age:
-        printf("\n\t\t\tAge: ");
-        scanf("%d", &p.age);
-
-        if(isalpha(p.age))
+        if (valid == 0)
         {
-            printf("\n\t Wrong Input Enter Again!!!");
-        }
-
-//***************************************Contact Number*********************************************
-    contact:
-        printf("\n\t\t\tContact Number: ");
-        scanf("%s", &p.Contact_no);
-
-        if(strlen(p.Contact_no)!=10)
-        {
-            printf("\n\t\t\tPlease Enter Atleast 10 digit for contact number");
+            printf("\n\t\t\tContact Number Contains Invalid Characters\nPlease Try Again");
             getch();
             system("cls");
             goto contact;
         }
-        else if(strlen(p.Contact_no)==10)
-        {
-            for(int i = 0; i<strlen(p.Contact_no); i++)
-            {
-                if(!isalpha(p.Contact_no[i]))
-                {
-                    valid = 1;
+    }
 
-                }
-                else
-                {
-                    valid = 0;
-                    break;
-                }
-            }
-            if (valid == 0)
+    //********emali****************
+email:
+    printf("\n\t\t\tEmail ID: ");
+    scanf("%s", &p.email);
+
+    if (strlen(p.email) > 35 || strlen(p.email) < 8)
+    {
+        printf("\n\t Invalid The Maximum and Minimum Range Of Email is 35 & 8 (Enter Again)");
+        goto email;
+    }
+
+    //**********Enrollment Number**************
+
+enrollment:
+    printf("\n\t\t\tEnrollment No: ");
+    scanf("%s", &p.Enrollment);
+    int b;
+    if (strlen(p.Enrollment) == 10)
+    {
+        for (b = 0; b < strlen(p.Enrollment); b++)
+        {
+            if (!isalpha(p.Enrollment[0]))
             {
-                printf("\n\t\t\tContact Number Contains Invalid Characters\nPlease Try Again");
+                valid = 1;
+            }
+            else
+            {
+                valid = 0;
+                break;
+            }
+            if (!valid)
+            {
+                printf("\n\t Enrollment NO Contains Invalid Character \t TRY AGAIN");
                 getch();
-                system("cls");
-                goto contact;
+                goto enrollment;
             }
         }
-
-//*************************emali***********************************************
-    email:
-        printf("\n\t\t\tEmail ID: ");
-        scanf("%s", &p.email);
-
-        if(strlen(p.email)>35 || strlen(p.email)<8)
-        {
-            printf("\n\t Invalid The Maximum and Minimum Range Of Email is 35 & 8 (Enter Again)");
-            goto email;
-        }
-
-//**************************branch**********************************************
-    branch:
-        printf("\n\t\t\tBranch: ");
-        scanf("%s", &p.branch);
-
-        if(strlen(p.branch)>20 || strlen(p.branch)<1)
-        {
-            printf("\n\t Invalid The Maximum and Minimum Range Of Branch is 20 & 0 (Enter Again)");
-            p.branch[0] = toupper(p.branch[0]);
-            getch();
-            goto branch;
-        }
-
-//***************************Enrollment Number*****************************************
-
-    Enrollment:
-        printf("\n\t\t\tEnrollment Number: ");
-        scanf("%u", &p.enr_num);
-
-        if(isalpha(p.enr_num))
-        {
-            printf("\n\t\t\tPlease Enter Valid Number.\tTry Again");
-            getch();
-            goto Enrollment;
-        }
-
-
-        fprintf(data,"%s %s %s %d %s %s %s %u",p.First_Name, p.Last_Name, p.Gender, p.age, p.Contact_no, p.email, p.branch, p.enr_num );
-        printf("\n\t\t\tInformation Filled Successfully!");
-		fclose(data);
+    }
+    else
+    {
+        printf("\n\t Sorry Enrollment no No Must Contain 10 Numbers !!!!");
         getch();
-        while(1)
-        {
-            printf("\n\t\tDo You Want To Add More Record?(Y/N): ");
-            fflush(stdin);
-            scanf("%c", &ans);
-            switch(ans)
-            {
-            case 'y': case 'Y':
-                system("cls");
-                add_rec();
-                break;
+        goto enrollment;
+    }
 
-            case 'n': case 'N':
-                system("pause");
-                main_menu();
-                break;
+    /// ************************************** Department ***********************************
 
-            default:
-                printf("\n\t\t\tInvalid Input, Please Try Again.");
-                break;
-
-            }
+department:
+    printf("\n\t\t\t Department :- ");
+    scanf("%s", &p.department1);
+    p.department1[0] = toupper(p.department1[0]);
+    char pp;
+    noni:
+    printf("\n\n\n \t\t\t  ARE YOU SURE YOU WANT TO ENTER THIS DETAILS [Y/N] :- ");
+    pp = getche();
+    if (pp == 'y' || pp == 'Y')
+    {
+        fwrite(&p, sizeof(struct student), 1, data);
+        printf("\n\n\n\t\t\tInformation Filled Successfully!");
+        fclose(data);
+        getch();
+    }
+    else{
+        if(pp == 'n'|| pp == 'N'){
+              printf("\n\n\t\t\t Ok !!! Enter Information AGain\n\n");
+              system("pause");
+              add_rec();
+        }else{
+            printf("\n\n\t\t\t Please Enter Valid Charecter !!!!!");
+            getch();
+            goto noni;
         }
+    }
+
+    while (1)
+    {
+        printf("\n\t\tDo You Want To Add More Record?(Y/N): ");
+        fflush(stdin);
+        scanf("%c", &ans);
+        switch (ans)
+        {
+        case 'y':
+        case 'Y':
+            system("cls");
+            add_rec();
+            break;
+
+        case 'n':
+        case 'N':
+            system("pause");
+            main_menu();
+            break;
+
+        default:
+            printf("\n\t\t\tInvalid Input, Please Try Again.");
+            break;
+        }
+    }
 }
 
 void view_records()
@@ -360,49 +396,127 @@ void view_records()
     printf("\t-----------------------------------------------------------------\n");
 
     FILE *data;
-    data = fopen("studnt.dat", "r");
+    data = fopen("studnt.bin", "rb");
     gotoxy(1, 10);
     printf("Name");
-
     gotoxy(20, 10);
     printf("Gender");
-
     gotoxy(32, 10);
     printf("Age");
-
-    gotoxy(37, 10);
+    gotoxy(45, 10);
     printf("Contact Number");
-
-    gotoxy(49, 10);
+    gotoxy(79, 10);
     printf("Email");
-
-    gotoxy(64, 10);
-    printf("Branch");
-
-    gotoxy(88, 10);
-    printf("Enrollment Number\n");
-    printf("**********************************************************************************************************************\n");
-    row = 17;
-    while(fscanf(data, "%s %s %s %d %s %s %s %u", p.First_Name, p.Last_Name, p.Gender, p.age, p.Contact_no, p.email, p.branch, p.enr_num))
+    gotoxy(100, 10);
+    printf("Enrollment");
+    gotoxy(130, 10);
+    printf("Department\n");
+    printf("************************************************************************************************************************\n");
+    row = 13;
+    //while(fscanf(data, "%s %s %s %d %s %s %s %u", p.First_Name, p.Last_Name, p.Gender, p.age, p.Contact_no, p.email, p.branch, p.enr_num))
+    while (fread(&p, sizeof(struct student), 1, data))
     {
         gotoxy(1, row);
         printf("%s %s", p.First_Name, p.Last_Name);
-        gotoxy(20, row);
+        gotoxy(22, row);
         printf("%s", p.Gender);
         gotoxy(32, row);
         printf("%d", p.age);
-        gotoxy(37, row);
+        gotoxy(45, row);
         printf("%s", p.Contact_no);
-        gotoxy(49, row);
+        gotoxy(68, row);
         printf("%s", p.email);
-        gotoxy(64, row);
-        printf("%s", p.branch);
-        gotoxy(88, row);
-        printf("%u", p.enr_num);
+        gotoxy(100, row);
+        printf("%s", p.Enrollment);
+        gotoxy(130, row);
+        printf("%s", p.department1);
+        row++;
         row++;
     }
     fclose(data);
     printf("\n\t\t");
-    system("pause");
+    getch();
     main_menu();
+}
+void search_rec()
+{
+    char name[30];
+    title();
+    FILE *data;
+    data = fopen("studnt.bin", "rb");
+    printf("\n\n\n\t\t\t ******************* Search Student Record *******************\n\n");
+    printf("Enter Student's First Name :- ");
+    scanf("%s", name);
+    fflush(stdin);
+    system("cls");
+    title();
+    name[0] = toupper(name[0]);
+    while (fread(&p, sizeof(struct student), 1, data))
+    {
+        if (strcmp(p.First_Name, name) == 0)
+        {
+            gotoxy(1, 10);
+            printf("Name");
+            gotoxy(20, 10);
+            printf("Gender");
+            gotoxy(32, 10);
+            printf("Age");
+            gotoxy(45, 10);
+            printf("Contact Number");
+            gotoxy(79, 10);
+            printf("Email");
+            gotoxy(100, 10);
+            printf("Enrollment");
+            gotoxy(130, 10);
+            printf("Department\n");
+            printf("************************************************************************************************************************\n");
+            gotoxy(1, 13);
+            printf("%s %s", p.First_Name, p.Last_Name);
+            gotoxy(22, 13);
+            printf("%s", p.Gender);
+            gotoxy(32, 13);
+            printf("%d", p.age);
+            gotoxy(45, 13);
+            printf("%s", p.Contact_no);
+            gotoxy(68, 13);
+            printf("%s", p.email);
+            gotoxy(100, 13);
+            printf("%s", p.Enrollment);
+            gotoxy(130, 13);
+            printf("%s", p.department1);
+            printf("\n");
+            break;
+            
+        }
+    }
+    if (strcmp(p.First_Name, name) != 0)
+    {
+        gotoxy(5, 10);
+        printf("Record Not Found");
+        getch();
+    }
+    fclose(data);
+L:
+    getch();
+    char and;
+    printf("\n\n\n\t\t Do You Want TO Veiw More[Y/N]? :- ");
+    scanf("%c", &and);
+    switch (and)
+    {
+    case 'y':
+    case 'Y':
+        system("cls");
+        search_rec();
+        break;
+
+    case 'n':
+    case 'N':
+        system("pause");
+        main_menu();
+        break;
+
+    default:
+        printf("\n\t\t\tInvalid Input, Please Try Again.");
+        break;
+    }
 }
